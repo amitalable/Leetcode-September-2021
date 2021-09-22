73032843f77f1d46001f0fbc6ff7e4218128655a
amitalable <amitalable@gmail.com>
2021-09-22 22:07:12 +0530

    Day 22

---
+++ LeetcodeSeptember/Week-4/MaxLength.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/maximum-length-of-a-concatenated-string-with-unique-characters
+class Solution:
+    def dfs(self, arr, pos, res):
+        if len(res) != len(set(res)):
+            return 0
+        best = len(res)
+        for i in range(pos,len(arr)):
+            best = max(best,self.dfs(arr,i+1,res+arr[i]))
+        return best
+    
+    def maxLength(self, arr: List[str]) -> int:
+        return self.dfs(arr,0,"")
+    
