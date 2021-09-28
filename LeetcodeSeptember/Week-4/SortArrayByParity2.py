572df385628af144c03a559c7e879535f4df89c1
amitalable <amitalable@gmail.com>
2021-09-28 22:28:08 +0530

    Day 28

---
+++ LeetcodeSeptember/Week-4/SortArrayByParity2.py
@@ -0,0 +1,18 @@
+# https://leetcode.com/problems/sort-array-by-parity-ii
+
+from typing import List
+
+
+class Solution:
+    def sortArrayByParityII(self, nums: List[int]) -> List[int]:
+        
+        i, j, n = 0, 1, len(nums)
+        while j < n and i < n:
+            if nums[i] % 2 == 0:
+                i += 2
+            elif nums[j] % 2 == 1:
+                j += 2
+            else:
+                nums[i], nums[j] = nums[j], nums[i]
+        return nums
+                    
